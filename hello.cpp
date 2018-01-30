923ba35cf4d4347766fa7522eb002caa7f629877
eruveyarceta <earceta@mail.csuchico.edu>
2018-01-30 11:35:01 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Eruvey.What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
